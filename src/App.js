c140304afb5d5e25a6a531f02153d9d6ad0f63cb
sudonona <80674485+sudonona@users.noreply.github.com>
2024-08-30 16:40:58 +0200

    adds

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import './App.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MyNavbar from './Components/MyNavbar';
 import MyFooter from './Components/MyFooter';
 import Search from './Components/Search';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import CityDetails from './Components/CityDetail';
+import NotFound from './Components/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <header>
-          <MyNavbar />
+          
+          <MyNavbar className="py-1" />
         </header>
+        
         <main className="home">
+         
           <Search />
+
+         
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/previsioni" element={<CityDetails />} />
+            <Route path="/" element={<Home />} /> 
+            <Route path="/previsioni" element={<CityDetails />} /> 
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
+
+       
         <footer>
           <MyFooter />
         </footer>
